f16ee3e1560154e2414bddf2a998613055ec0618
Alex Rodionov <p0deje@gmail.com>
2015-03-11 12:21:29 +0600

    Fix rename file spec

--- spec/plugin/renaming_files_spec.rb
+++ spec/plugin/renaming_files_spec.rb
@@ -2,33 +2,22 @@ require 'spec_helper'
 
 describe "Renaming files" do
   it "can rename files" do
-    write_file 'one.txt', 'One'
+    write_file 'one.txt', 'One Two'
     write_file 'two.txt', 'Two'
 
-    vim.set_buffer_contents normalize_string_indent(<<-EOF)
-      one.txt:1:One
-      --
-      two.txt:1:Two
-    EOF
-    vim.command 'call writable_search#Parse()'
+    vim.command 'WritableSearch Two'
 
     vim.command '%s/two.txt/three.txt/g'
     vim.write
 
-    IO.read('one.txt').strip.should eq 'One'
-    File.exists?('two.txt').should be_false
+    IO.read('one.txt').strip.should eq 'One Two'
+    File.exists?('two.txt').should eq false
     IO.read('three.txt').strip.should eq 'Two'
   end
 
   it "can handle two renames of a single file" do
     write_file 'one.txt', "One\nOne"
-
-    vim.set_buffer_contents normalize_string_indent(<<-EOF)
-      one.txt:1:One
-      --
-      one.txt:2:One
-    EOF
-    vim.command 'call writable_search#Parse()'
+    vim.command 'WritableSearch One'
 
     vim.command '%s/one.txt/two.txt/g'
     vim.command '%s/One/Two/g'
@@ -36,26 +25,21 @@ describe "Renaming files" do
     vim.write
 
     IO.read('two.txt').strip.should eq "Two\nTwo"
-    File.exists?('one.txt').should be_false
+    File.exists?('one.txt').should eq false
   end
 
   it "can move files to new directories" do
     FileUtils.mkdir('dir')
-    write_file 'dir/one.txt', 'One'
+    write_file 'dir/one.txt', 'One Two'
     write_file 'dir/two.txt', 'Two'
 
-    vim.set_buffer_contents normalize_string_indent(<<-EOF)
-      dir/one.txt:1:One
-      --
-      dir/two.txt:1:Two
-    EOF
-    vim.command 'call writable_search#Parse()'
+    vim.command 'WritableSearch Two'
 
     vim.command '%s?dir/two.txt?other_dir/three.txt?g'
     vim.write
 
-    IO.read('dir/one.txt').strip.should eq 'One'
-    File.exists?('dir/two.txt').should be_false
+    IO.read('dir/one.txt').strip.should eq 'One Two'
+    File.exists?('dir/two.txt').should eq false
     IO.read('other_dir/three.txt').strip.should eq 'Two'
   end
 end
