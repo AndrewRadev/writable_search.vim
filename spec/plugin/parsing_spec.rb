5dd96a5212b9be23956c099882b4fe8a0638fc2a
Andrew Radev <andrey.radev@gmail.com>
2020-04-16 10:39:33 +0300

    Fix tests

--- spec/plugin/parsing_spec.rb
+++ spec/plugin/parsing_spec.rb
@@ -1,32 +1,52 @@
 require 'spec_helper'
 
 describe "Parsing" do
+  before :each do
+    FileUtils.mkdir_p('autoload/writable_search')
+
+    write_file 'autoload/writable_search/proxy.vim', <<~EOF
+      " padding
+      " to adjust next proxies.
+      function! writable_search#proxy#UpdateSource(new_lines, adjustment) dict
+
+    EOF
+    write_file 'autoload/writable_search/parser.vim', <<~EOF
+      function! writable_search#parser#Run()
+        let grouped_lines = s:PartitionLines(getbufline('%', 1, '$'))
+
+        "...
+        for lines in a:grouped_lines
+          let current_proxy          = writable_search#proxy#New(bufnr('%'))
+          let current_proxy.filename = s:FindFilename(lines)
+    EOF
+  end
+
   it "parses grep results into a different representation (separated by dashes)" do
     write_file 'grep_results', <<~EOF
-      autoload/writable_search/proxy.vim-26-" to adjust next proxies.
-      autoload/writable_search/proxy.vim:27:function! writable_search#proxy#UpdateSource(new_lines, adjustment) dict
-      autoload/writable_search/proxy.vim-28-  let new_lines = a:new_lines
+      autoload/writable_search/proxy.vim-2-" to adjust next proxies.
+      autoload/writable_search/proxy.vim:3:function! writable_search#proxy#UpdateSource(new_lines, adjustment) dict
+      autoload/writable_search/proxy.vim-4-
       --
       autoload/writable_search/parser.vim:1:function! writable_search#parser#Run()
       autoload/writable_search/parser.vim-2-  let grouped_lines = s:PartitionLines(getbufline('%', 1, '$'))
       --
-      autoload/writable_search/parser.vim-25-  for lines in a:grouped_lines
-      autoload/writable_search/parser.vim:26:    let current_proxy          = writable_search#proxy#New(bufnr('%'))
-      autoload/writable_search/parser.vim-27-    let current_proxy.filename = s:FindFilename(lines)
+      autoload/writable_search/parser.vim-5-  for lines in a:grouped_lines
+      autoload/writable_search/parser.vim:6:    let current_proxy          = writable_search#proxy#New(bufnr('%'))
+      autoload/writable_search/parser.vim-7-    let current_proxy.filename = s:FindFilename(lines)
     EOF
 
     vim.edit 'grep_results'
     vim.command 'call writable_search#Parse()'
 
-    expect(vim.buffer_contents).to eq <<~EOF.strip
-      autoload/writable_search/proxy.vim:26-28
+    expect(vim.buffer_contents.lines.map(&:strip)).to eq <<~EOF.lines.map(&:strip)
+      autoload/writable_search/proxy.vim:2-4
        " to adjust next proxies.
        function! writable_search#proxy#UpdateSource(new_lines, adjustment) dict
-         let new_lines = a:new_lines
+
       autoload/writable_search/parser.vim:1-2
        function! writable_search#parser#Run()
          let grouped_lines = s:PartitionLines(getbufline('%', 1, '$'))
-      autoload/writable_search/parser.vim:25-27
+      autoload/writable_search/parser.vim:5-7
          for lines in a:grouped_lines
            let current_proxy          = writable_search#proxy#New(bufnr('%'))
            let current_proxy.filename = s:FindFilename(lines)
@@ -35,27 +55,28 @@ describe "Parsing" do
 
   it "parses grep results into a different representation (no dashes)" do
     write_file 'grep_results', <<~EOF
-      autoload/writable_search/proxy.vim-26-" to adjust next proxies.
-      autoload/writable_search/proxy.vim:27:function! writable_search#proxy#UpdateSource(new_lines, adjustment) dict
-      autoload/writable_search/proxy.vim-28-  let new_lines = a:new_lines
+      autoload/writable_search/proxy.vim-2-" to adjust next proxies.
+      autoload/writable_search/proxy.vim:3:function! writable_search#proxy#UpdateSource(new_lines, adjustment) dict
+      autoload/writable_search/proxy.vim-4-
       autoload/writable_search/parser.vim:1:function! writable_search#parser#Run()
       autoload/writable_search/parser.vim-2-  let grouped_lines = s:PartitionLines(getbufline('%', 1, '$'))
-      autoload/writable_search/parser.vim-25-  for lines in a:grouped_lines
-      autoload/writable_search/parser.vim:26:    let current_proxy          = writable_search#proxy#New(bufnr('%'))
-      autoload/writable_search/parser.vim-27-    let current_proxy.filename = s:FindFilename(lines)
+      autoload/writable_search/parser.vim-5-  for lines in a:grouped_lines
+      autoload/writable_search/parser.vim:6:    let current_proxy          = writable_search#proxy#New(bufnr('%'))
+      autoload/writable_search/parser.vim-7-    let current_proxy.filename = s:FindFilename(lines)
     EOF
 
     vim.edit 'grep_results'
     vim.command 'call writable_search#Parse()'
-    expect(vim.buffer_contents).to eq <<~EOF.strip
-      autoload/writable_search/proxy.vim:26-28
+
+    expect(vim.buffer_contents.lines.map(&:strip)).to eq <<~EOF.lines.map(&:strip)
+      autoload/writable_search/proxy.vim:2-4
        " to adjust next proxies.
        function! writable_search#proxy#UpdateSource(new_lines, adjustment) dict
-         let new_lines = a:new_lines
+
       autoload/writable_search/parser.vim:1-2
        function! writable_search#parser#Run()
          let grouped_lines = s:PartitionLines(getbufline('%', 1, '$'))
-      autoload/writable_search/parser.vim:25-27
+      autoload/writable_search/parser.vim:5-7
          for lines in a:grouped_lines
            let current_proxy          = writable_search#proxy#New(bufnr('%'))
            let current_proxy.filename = s:FindFilename(lines)
